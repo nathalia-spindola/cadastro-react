384ea36dbc7317ea24ac84464d38b8e21287cda1
Nathalia Pauline Spindola <nathaliapaulinespindola@gmail.com>
2022-09-18 14:03:49 -0300

    Deixei bonitinho

--- cadastro/src/pages/TabDadosDeEndereco.tsx
+++ cadastro/src/pages/TabDadosDeEndereco.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import InputCep from "../components/inputCep";
+import InputCep from "../components/InputCep";
 import InputCidades from "../components/InputCidades";
 import InputEstados from "../components/InputEstados";
 
@@ -7,15 +7,18 @@ export default function() {
     const [uf, setUf] = useState("")
 
     return <>
-        <h1>Cadastro</h1>
-        <h2>Dados de Endereço</h2>
-        <InputCep />
-        <InputEstados setUf={setUf} />
-        <InputCidades uf={uf} />
-        <input placeholder="Bairro"></input>
-        <input placeholder="Rua"></input>
-        <input placeholder="Número"></input>
-        <input placeholder="Complemento"></input>
-        
+        <div>
+            <h1>Cadastro</h1>
+            <h2>Dados de Endereço</h2>
+            <InputCep/>
+            <div className="info">
+                <InputEstados setUf={setUf} />
+                <InputCidades uf={uf} />
+                <input placeholder="Bairro"></input>
+                <input placeholder="Rua"></input>
+                <input placeholder="Número"></input>
+                <input placeholder="Complemento"></input>
+            </div>
+        </div>
     </>
 }
